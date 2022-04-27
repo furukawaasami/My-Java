0b96a716c307e13cc42761541f00b1d03cbdd328
furukawaasami <98880369+furukawaasami@users.noreply.github.com>
2022-04-27 23:32:18 +0900

    javastart

---
+++ Greeting.java
@@ -0,0 +1,18 @@
+class Greeting {
+  public static void main(String args[]) {
+    System.out.println("Good evening Nana");
+    System.out.println("Good afternoon Nana");
+    System.out.println("Good morning Nana");
+
+    int var = 1;
+    int var01 = 2;
+    // int var-01 = 3;
+    // int 01var$01 = 4;
+
+    System.out.println(var);
+    System.out.println(var01);
+    // System.out.println(var-01);
+    // System.out.println(01var$01);
+
+  }
+}
