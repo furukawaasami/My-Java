f04c7f1afefe5d809acec31a19adb7d42d7ccf12
furukawaasami <98880369+furukawaasami@users.noreply.github.com>
2022-05-03 15:18:49 +0900

    Java超入門コース 合併版【Javaの超基本的な部分をたった1時間で学べます】【プログラミング初心者向け入門講座】47

---
+++ Lesson11_01.java
@@ -0,0 +1,24 @@
+public class Lesson11_01 {
+  public static void main(String[] args) {
+    int arr[] = { 2, 4, 6, 8, 10 };
+    int sum = 0;
+    for (int i = 0; i <= 3; i++) {
+      // if (i == 3) {
+      // break;
+      // continue;
+      // }
+      sum += arr[i];
+      // System.out.println(arr[i]);
+      System.out.println(sum);
+      
+    }
+
+    for (int j = 0; j <= 2; j++) {
+      for (int k = 0; k <= 4; k++) {
+        System.out.println(j + "_" + k);
+
+      }
+    }
+
+  }
+}
