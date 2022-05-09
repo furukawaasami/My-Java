fbb40a895ec07f765845341f6414dd9a7c7c802a
furukawaasami <98880369+furukawaasami@users.noreply.github.com>
2022-05-09 11:45:20 +0900

    5/9

--- Lesson13_01.java
+++ Lesson13_01.java
@@ -1,16 +1,20 @@
-class Student{
+class Student {
   String name = "Damian";
-  public void calculateAvg(int math,int english){
-    System.out.println((math + english)/2);
+
+  public void calculateAvg(int math, int english) {
+    System.out.println((math + english) / 2);
   }
 }
 
 public class Lesson13_01 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Student a001 = new Student();
     a001.name = "Damian";
 
     System.out.println(a001.name);
+    a001.calculateAvg(70, 50);
+    // System.out.println(a001.calculateAvg(70, 50));
+
   }
 
 }
